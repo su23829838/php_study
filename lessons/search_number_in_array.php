d7753c3a2d3ca4e43c779b748ec942f27c22d2da
su23829838 <104680234+su23829838@users.noreply.github.com>
2024-06-30 17:17:24 +0900

    Update search_number_in_array.php
    add code search index alter

--- lessons/search_number_in_array.php
+++ lessons/search_number_in_array.php
@@ -14,3 +14,27 @@
      }
     echo $ans;
 ?>
+
+// search index alter 
+<?php
+     $array_room_number = explode(" ",fgets(STDIN));
+    //入力からもらった数値分反復する
+    $array = [];
+    for($i = 0 ; $i < intval($array_room_number[0]); $i ++){
+           $array[] = intval(trim(fgets(STDIN)));  
+    }
+    
+    $ans = -1;
+     //配列の中に指定の数値がないか検索
+     
+     foreach ($array as $key => $value)
+     {
+         // echo $key+1; 
+         if($value == $array_room_number[1]){
+           $ans = $key+1;
+           break; 
+         }
+     }
+     
+    echo $ans;
+?>
