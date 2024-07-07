401202c0e36a8900c446805249fcecc5769dd582
su23829838 <104680234+su23829838@users.noreply.github.com>
2024-07-08 03:15:08 +0900

    Update array_lessons.php
    add fix

--- lessons/array_lessons.php
+++ lessons/array_lessons.php
@@ -70,3 +70,28 @@
     }
     
 ?>
+
+// revese array
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $counter = intval(fgets(STDIN));
+    $array = [];
+    for($i = 0 ; $i < $counter; $i++){
+      $array []=  intval(fgets(STDIN));
+    }
+    
+    $array_R = array_reverse($array);
+    
+    foreach ($array_R as  $item){
+                 print($item)."\n";
+    }
+?>
+
+// swap variable
+<?php
+    $numbers = explode(" ", trim(fgets(STDIN)));
+    $a = $numbers[1];
+    $b = $numbers[0];
+    echo $a ." " .$b;
+?>
