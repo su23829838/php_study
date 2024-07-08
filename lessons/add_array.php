5d1a5b1ea7ee1b2581d2593c3768df1a94ce4b24
su23829838 <104680234+su23829838@users.noreply.github.com>
2024-07-08 16:43:48 +0900

    Update add_array.php
    array_ splice add

--- lessons/add_array.php
+++ lessons/add_array.php
@@ -7,3 +7,30 @@
     //配列の合計を計算
     echo   array_sum($array);
 ?>
+
+//array add
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+ $count = fgets(STDIN);
+  $array = [];
+  for($i=0; $i < $count; $i++)
+  {
+      $array [] = intval(fgets(STDIN));   
+  }
+  
+   //print_r($array); 
+  $target = explode(" ",fgets(STDIN));
+ 
+ //print_r($target);
+  $t1= $target[0];
+  $t2= $target[1];
+  
+ //$array[$t1] = $t2;
+ array_splice($array,  $t1, 0, $t2); 
+   
+   foreach($array as $item){
+      echo $item . "\n";
+   }
+
+?>
